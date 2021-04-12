4f5bd3564d6a466a0432e972dfe20d6cefd48325
Tim Chou - TW (SW) <TimChou@supermicro.com>
2021-04-12 17:55:52 +0800

    tooltip

--- src/app/my-module/my-component/my-component.component.ts
+++ src/app/my-module/my-component/my-component.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NzIconService } from 'ng-zorro-antd/icon';
 
+const ngZorrIconLiteral =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="17.625" height="21.198" viewBox="0 0 17.625 21.198"><defs><style>.a{fill:none;stroke:#fff;stroke-width:2px;}.b{fill:#fff;}</style></defs><g transform="translate(1.001 1.094)"><g transform="translate(0 0)"><g transform="translate(0 0)"><path class="a" d="M18.617,4.468,10.814,1,3.01,4.468v5.2a10.408,10.408,0,0,0,7.8,10.4c1.994-.486,8.022-3.258,7.8-10.4Z" transform="translate(-3 -1)"/></g></g><path class="b" d="M1,7.883a1,1,0,1,1,0-2H3.382l-.02-4.9A.975.975,0,0,1,4.344,0a1,1,0,0,1,.992.987l.025,5.9a.976.976,0,0,1-.847.97,1,1,0,0,1-.224.025Z" transform="translate(9.11 3.96) rotate(45)"/></g></svg>';
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
@@ -8,8 +11,13 @@ import { Component, OnInit } from '@angular/core';
 export class MyComponentComponent implements OnInit {
   isLoadingOne = false;
   isLoadingPowerOff = false;
+  color: string;
+  customColors: string[] = ['#f50', '#2db7f5', '#87d068', '#108ee9'];
 
-  constructor() {}
+  constructor(private iconService: NzIconService) {
+    this.iconService.addIconLiteral('ng-zorro:antd', ngZorrIconLiteral);
+    this.color = '#2db7f5';
+  }
 
   ngOnInit(): void {}
   loadOne() {
--- src/app/my-module/my-module.module.ts
+++ src/app/my-module/my-module.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { NzToolTipModule } from 'ng-zorro-antd';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
 @NgModule({
   declarations: [MyComponentComponent],
   imports: [
     CommonModule,
+    NzToolTipModule,
     NzButtonModule,
     NzDividerModule,
     NzIconModule,
